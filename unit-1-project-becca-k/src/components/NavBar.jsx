17e99d72c807790f20d5b93513f4dc03af81d291
beccakunce <rebeccakunce@gmail.com>
2026-09-17 23:33:55 -0500

    updating nav bar

--- unit-1-project-becca-k/src/components/NavBar.jsx
+++ unit-1-project-becca-k/src/components/NavBar.jsx
@@ -11,6 +11,7 @@ const NavBar = () => {
         <li><Link to="/CheckInPage">Let's Check In</Link></li>
         <li><Link to="/CreateYourOwnPage">Create Your Own Love Bomb</Link></li>
         <li><Link to="/PetLoveBombPage">Pet Love Bombs</Link></li>
+        <li><Link to="/CreateUserPage">Create a User</Link></li>
       </ul>
     </div>   
     </nav>
